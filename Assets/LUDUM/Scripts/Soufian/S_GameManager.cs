b8d7cc25b9a3d8fd8a968ad9a4f6ff4a9183a3af
Soufian <soufiazzouz@laposte.net>
2016-12-11 00:26:16 +0100

    Test arrivée ia

--- Assets/LUDUM/Scripts/Soufian/S_GameManager.cs
+++ Assets/LUDUM/Scripts/Soufian/S_GameManager.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class S_GameManager : S_GenericSingleton<S_GameManager>
 {
     [SerializeField] private GameObject m_goCamera;
+    [SerializeField] private float m_fTimeMax;
+    [SerializeField] private GameObject m_goIA;
+
     private S_CameraRaycast S_CameraRaycastComponent;
     private bool m_bGameLost;
+    private bool m_bGameFinished;
 
 	void Start ()
     {
         m_bGameLost = false;
+        m_bGameFinished = false;
         S_CameraRaycastComponent = m_goCamera.GetComponent<S_CameraRaycast>();
+        if (m_fTimeMax <= 0f)
+        {
+            m_fTimeMax = 60 * 2;
+        }
     }
 
     private void Update()
@@ -23,6 +32,8 @@ public class S_GameManager : S_GenericSingleton<S_GameManager>
         {
             Debug.Log("Nothing");
         }
+
+        TimerCountdown();
     }
 
     public void AskCameraToRaycast(Transform _tObjectTransform)
@@ -31,5 +42,26 @@ public class S_GameManager : S_GenericSingleton<S_GameManager>
         {
             m_bGameLost = true;
         }
+    }
+
+    private void TimerCountdown()
+    {
+        m_fTimeMax -= Time.deltaTime;
+        float fMinutes = Mathf.Floor(m_fTimeMax / 60);
+        float fSecondes = Mathf.Floor(m_fTimeMax - fMinutes * 60);
+        string sDisplayedMinutes = fMinutes.ToString("00");
+        string sDisplayedSecondes = fSecondes.ToString("00");
+        if (!m_bGameFinished)
+        {
+            if (m_fTimeMax <= 0f)
+            {
+                m_fTimeMax = 0f;
+                m_bGameFinished = true;
+                Instantiate(m_goIA, m_goIA.transform.position, m_goIA.transform.rotation);
+                //Play Animation
+                //Stop Player Movement
+            }
+        }
+        Debug.Log(sDisplayedMinutes + ":" + sDisplayedSecondes);
     }
 }
