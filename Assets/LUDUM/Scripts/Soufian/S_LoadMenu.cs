1ea567976a5054e0d552d67c79c959dee3acce3e
Soufian <soufiazzouz@laposte.net>
2016-12-10 23:07:35 +0100

    Menu ok ?

--- Assets/LUDUM/Scripts/Soufian/S_LoadMenu.cs
+++ Assets/LUDUM/Scripts/Soufian/S_LoadMenu.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 public class S_LoadMenu : MonoBehaviour
 {
     private bool m_bIsLoadingScene;
-    private HingeJoint m_hjHingeJointComponent;
+    //private HingeJoint m_hjHingeJointComponent;
 	// Use this for initialization
 	void Start ()
     {
         m_bIsLoadingScene = false;
-        m_hjHingeJointComponent = GetComponent<HingeJoint>();
+        //m_hjHingeJointComponent = GetComponent<HingeJoint>();
     }
 	
 	// Update is called once per frame
@@ -18,9 +18,11 @@ public class S_LoadMenu : MonoBehaviour
     {
 	    if(!m_bIsLoadingScene)
         {
-            //if(m_hjHingeJointComponent.)
-            SceneManager.LoadScene("S_Main");
-            m_bIsLoadingScene = true;
+            if (transform.localRotation.eulerAngles.y > 24f)
+            {
+                SceneManager.LoadScene("S_Main");
+                m_bIsLoadingScene = true;
+            }
         }
 	}
 }
