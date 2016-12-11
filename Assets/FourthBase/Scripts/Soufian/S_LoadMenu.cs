e93bbce97cebb17be3e7ad4a165f85388d42e527
Soufian <soufiazzouz@laposte.net>
2016-12-11 14:00:41 +0100

    MENU FUCK

--- Assets/FourthBase/Scripts/Soufian/S_LoadMenu.cs
+++ Assets/FourthBase/Scripts/Soufian/S_LoadMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 public class S_LoadMenu : MonoBehaviour
 {
     [SerializeField] private GameObject m_goCamera;
+    [SerializeField] private float m_fFadeDuration;
+    [SerializeField] private AudioSource m_asAudioSource;
 
     private bool m_bIsLoadingScene;
-    private AudioSource m_asAudioSource;
     //private HingeJoint m_hjHingeJointComponent;
 	// Use this for initialization
 	void Start ()
     {
-        m_asAudioSource = m_goCamera.GetComponent<AudioSource>();
         m_bIsLoadingScene = false;
         //m_hjHingeJointComponent = GetComponent<HingeJoint>();
     }
@@ -25,9 +25,9 @@ public class S_LoadMenu : MonoBehaviour
             if (transform.localRotation.eulerAngles.y > 24f)
             {
                 m_bIsLoadingScene = true;
-                SteamVR_Fade.Start(Color.black, 2f);
-                m_asAudioSource.Play();
-                Invoke("LoadMenu", m_asAudioSource.clip.length+1f);
+                SteamVR_Fade.Start(Color.black, m_fFadeDuration);
+                Invoke("PlayIntro", m_fFadeDuration + 1f);
+                Invoke("LoadMenu", m_asAudioSource.clip.length + m_fFadeDuration + 1f);
             }
         }
 	}
@@ -35,5 +35,10 @@ public class S_LoadMenu : MonoBehaviour
     void LoadMenu()
     {
         SceneManager.LoadScene("S_Main");
+    }
+
+    void PlayIntro()
+    {
+        m_asAudioSource.Play();
     }
 }
