54610679aaa8364beeb7623e45d0f5a796f7b3ca
Soufian <soufiazzouz@laposte.net>
2016-12-11 23:43:02 +0100

    Test optimisation

--- Assets/FourthBase/Scripts/Soufian/S_IsVisibleOrNot.cs
+++ Assets/FourthBase/Scripts/Soufian/S_IsVisibleOrNot.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class S_IsVisibleOrNot : MonoBehaviour
 {
+    private float m_fTime;
+    [SerializeField]
+    private float m_fFrequence;
 
     // Use this for initialization
     void Start ()
     {
-
+        m_fFrequence = 1f;
+        m_fTime = 0f;
     }
 
     void Update()
     {
-        OnBecameVisible();
+        if (m_fTime > m_fFrequence)
+        {
+            m_fTime = 0f;
+
+            OnBecameVisible();
+        }
+        m_fTime += Time.deltaTime;
     }
 
     private void OnBecameVisible()
