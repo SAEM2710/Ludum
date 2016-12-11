57de0d3487ccc4a669bf3b8b2dcc8985cb9c4ab2
Soufian <soufiazzouz@laposte.net>
2016-12-11 13:04:14 +0100

    Menu ok ?

--- Assets/FourthBase/Scripts/Soufian/S_LoadMenu.cs
+++ Assets/FourthBase/Scripts/Soufian/S_LoadMenu.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 public class S_LoadMenu : MonoBehaviour
 {
     private bool m_bIsLoadingScene;
+    private AudioSource m_asAudioSource;
     //private HingeJoint m_hjHingeJointComponent;
 	// Use this for initialization
 	void Start ()
     {
+        m_asAudioSource = Camera.main.GetComponent<AudioSource>();
         m_bIsLoadingScene = false;
         //m_hjHingeJointComponent = GetComponent<HingeJoint>();
     }
@@ -20,9 +22,16 @@ public class S_LoadMenu : MonoBehaviour
         {
             if (transform.localRotation.eulerAngles.y > 24f)
             {
-                SceneManager.LoadScene("S_Main");
                 m_bIsLoadingScene = true;
+                SteamVR_Fade.Start(Color.black, 2f);
+                m_asAudioSource.Play();
+                Invoke("LoadMenu", m_asAudioSource.clip.length+1f);
             }
         }
 	}
+
+    void LoadMenu()
+    {
+        SceneManager.LoadScene("S_Main");
+    }
 }
