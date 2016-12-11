25e3b3b273fac7f97cf71f21eaf077ba9c3801ae
Yslean <enjminbonnetedouard@gmail.com>
2016-12-11 02:20:10 +0100

    Test Lampe

---
+++ Assets/FourthBase/Scripts/Edouard/OfficeLamp.cs
@@ -0,0 +1,30 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class OfficeLamp : VRTK_InteractableObject
+    {
+        private bool m_IsOn;
+
+        private Light m_Light;
+
+        protected override void Awake()
+        {
+            m_Light = GetComponent<Light>();
+
+            m_Light.gameObject.SetActive(false);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+
+            m_Light.gameObject.SetActive(!m_Light.gameObject.activeSelf);
+        }
+    }
+}
