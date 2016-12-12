fa875a998a9e7dc23c30667cb5c1ace84aec1697
Foehn Gallet <foehn.gallet@gmail.com>
2016-12-13 00:55:59 +0100

    Final ou presque

--- Assets/FourthBase/Scripts/Edouard/E_Objects.cs
+++ Assets/FourthBase/Scripts/Edouard/E_Objects.cs
@@ -10,7 +10,9 @@
 
         protected override void Awake()
         {
+            base.Awake();
             m_AudioSource = GetComponent<AudioSource>();
+            m_AudioSource.spatialBlend = 1;
         }
 
         protected override void Start()
@@ -18,15 +20,16 @@
             base.Start();
         }
 
-        public override void StartUsing(GameObject usingObject)
+        public override void Grabbed(GameObject usingObject)
         {
             base.StartUsing(usingObject);
-
+            m_AudioSource.volume = 0.5f;
             m_AudioSource.PlayOneShot(m_Grabbed[Random.Range(0, m_Grabbed.Length -1)]);
         }
 
         void OnCollisionEnter(Collision other)
         {
+            m_AudioSource.volume = 0.2f;
             m_AudioSource.PlayOneShot(m_Dropped[Random.Range(0, m_Dropped.Length -1)]);
         }
     }
