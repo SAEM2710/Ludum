223a24632d3d2759b9dd152332f38d84a8d98d59
Yslean <enjminbonnetedouard@gmail.com>
2016-12-12 02:37:43 +0100

    Musique + Dad Routine + Victory + ...

---
+++ Assets/FourthBase/Scripts/Edouard/Radio.cs
@@ -0,0 +1,34 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Radio : VRTK_InteractableObject
+    {
+        private bool m_IsOn = true;
+
+        private AudioSource m_Music;
+
+        protected override void Awake()
+        {
+            m_Music = GetComponent<AudioSource>();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+
+            m_IsOn = !m_IsOn;
+
+            if(m_IsOn)
+            {
+                m_Music.Pause();
+            }
+            else { m_Music.UnPause(); }
+        }
+    }
+}
